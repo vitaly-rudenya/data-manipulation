43bc93d043c5d3c9561379f7024d0864052bf726
mobius <mobiusp@rambler.ru>
2016-06-05 19:56:51 +0300

    Main class initial commit

---
+++ manipulation-app/src/main/java/com/dyggyty/manipulation/ManipulationApp.java
@@ -0,0 +1,11 @@
+package com.dyggyty.manipulation;
+
+/**
+ * Main data manipulation application class.
+ */
+public class ManipulationApp {
+
+    public static void main(String[] args) {
+
+    }
+}
